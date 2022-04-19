44f81b3feb48c49c147c34f351669d5c2cfa6cd9
玄学炼丹师 <77034561+CPones@users.noreply.github.com>
2022-04-19 22:13:10 +0800

    Update train.py

--- train.py
+++ train.py
@@ -26,7 +26,7 @@ def parse_args():
     parser.add_argument('--batch_size', type=int, default=64, help='batch size in training')
     parser.add_argument('--model', default='pointnet_cls', help='model name [default: pointnet_cls]')
     parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
-    parser.add_argument('--epoch', default=200, type=int, help='number of epoch in training')
+    parser.add_argument('--epoch', default=100, type=int, help='number of epoch in training')
     parser.add_argument('--learning_rate', default=0.001, type=float, help='learning rate in training')
     parser.add_argument('--num_point', type=int, default=1024, help='Point Number')
     parser.add_argument('--optimizer', type=str, default='Adam', help='optimizer for training')
